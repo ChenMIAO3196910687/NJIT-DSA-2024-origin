14f3289420f5c068089f6ec8c9b8a15cfa91bd03
ChenMIAO3196910687 <126169856+ChenMIAO3196910687@users.noreply.github.com>
2024-04-03 21:00:06 +0800

    Algorithms.java

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,57 +1,81 @@
 package oy.tol.tra;
 
 public class Algorithms {
-
+    
     public static <T extends Comparable<T>> void sort(T[] array) {
-        boolean swapped = true;
         int n = array.length;
-
-        while (swapped) {
+        boolean swapped;
+        
+        do {
             swapped = false;
             for (int i = 1; i < n; i++) {
                 if (array[i - 1].compareTo(array[i]) > 0) {
-                    T temp = array[i - 1];
-                    array[i - 1] = array[i];
-                    array[i] = temp;
+                    swap(array, i - 1, i);
                     swapped = true;
                 }
             }
-            n--;
-        }
+        } while (swapped);
     }
 
     public static <T> void reverse(T[] array) {
         int left = 0;
         int right = array.length - 1;
-        
+
         while (left < right) {
-            T temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
+            swap(array, left, right);
             left++;
             right--;
         }
     }
-
+    
     public static <T extends Comparable<T>> int binarySearch(T value, T[] array, int fromIndex, int toIndex) {
-        if (array == null || fromIndex < 0 || toIndex > array.length || fromIndex >= toIndex) {
-            throw new IllegalArgumentException("Invalid array or range");
-        }
-
-        while (fromIndex <= toIndex) {
-            int midIndex = (fromIndex + toIndex) / 2;
-            T midValue = array[midIndex];
-
-            int comparison = value.compareTo(midValue);
-            if (comparison == 0) {
-                return midIndex;
-            } else if (comparison < 0) {
-                toIndex = midIndex - 1;
+        if (fromIndex <= toIndex) {
+            int mid = fromIndex + (toIndex - fromIndex) / 2;
+            int comparisonResult = value.compareTo(array[mid]);
+    
+            if (comparisonResult == 0) {
+                return mid; 
+            } else if (comparisonResult < 0) {
+                return binarySearch(value, array, fromIndex, mid - 1);  
             } else {
-                fromIndex = midIndex + 1;
+                return binarySearch(value, array, mid + 1, toIndex); 
+            }
+        }
+    
+        return -1; 
+    }
+    
+    public static <E extends Comparable<E>> void fastSort(E[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void quickSort(E[] array, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+            quickSort(array, begin, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    private static <E extends Comparable<E>> int partition(E[] array, int begin, int end) {
+        E pivot = array[end];
+        int i = begin - 1;
+
+        for (int j = begin; j < end; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                i++;
+                swap(array, i, j);
             }
         }
 
-        return -1;
+        swap(array, i + 1, end);
+        return i + 1;
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
+
